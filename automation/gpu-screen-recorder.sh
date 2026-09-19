d014ae8c84460b324e75561b286520f1de5e3ab0
Zack <zedd9001@proton.me>
2026-09-19 05:33:27 +0500

    Add `gpu-screen-recorder` wrapper script to automation

---
+++ automation/gpu-screen-recorder.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Clip the last 15 seconds like how medal does it BUT for linux and store it locally
+
+
+CLIP_DIR="$HOME/Videos/Clips"
+
+mkdir -p "$CLIP_DIR"
+
+if pgrep -f "^gpu-screen-recorder " >/dev/null; then
+    echo "GPU Screen Recorder is already running."
+    exit 0
+fi
+
+exec gpu-screen-recorder \
+    -w edP-1 \
+    -r 15 \
+    -f 30 \
+    -encoder cpu \
+    -q medium \
+    -tune performance \
+    -a default_output \
+    -c mp4 \
+    -o "$HOME/Videos/Clips"
